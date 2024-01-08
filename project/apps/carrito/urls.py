e56ab83a5cb4cd106607e1f79524ef2070c99483
Tomas Greco <grecotomas06@gmail.com>
2024-01-08 12:03:51 -0300

    Aplicacion Carrito

--- project/apps/carrito/urls.py
+++ project/apps/carrito/urls.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from apps.carrito.views import tienda, agregar_producto, eliminar_producto, restar_producto, limpiar_carrito
 
-
+urlpatterns = [
+    path("carrito/", tienda, name="carrito"),
+    path('agregar/<int:producto_id>/', agregar_producto, name="Add"),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name="Del"),
+    path('restar/<int:producto_id>/', restar_producto, name="Sub"),
+    path('limpiar/', limpiar_carrito, name="CLS"),
+]
--- project/apps/carrito/views.py
+++ project/apps/carrito/views.py
@@ -7,30 +7,30 @@ from productos.models import Producto
 
 def tienda(request):
     productos = Producto.objects.all()
-    return render(request, "tienda.html", {'productos':productos})
+    return render(request, "carrito.html", {'productos':productos})
 
 def agregar_producto(request, producto_id):
     carrito = Carrito(request)
     producto = Producto.objects.get(id=producto_id)
     carrito.agregar(producto)
-    return redirect("Tienda")
+    return redirect("carrito:carrito")
 
 def eliminar_producto(request, producto_id):
     carrito = Carrito(request)
     producto = Producto.objects.get(id=producto_id)
     carrito.eliminar(producto)
-    return redirect("Tienda")
+    return redirect("carrito:carrito")
 
 def restar_producto(request, producto_id):
     carrito = Carrito(request)
     producto = Producto.objects.get(id=producto_id)
     carrito.restar(producto)
-    return redirect("Tienda")
+    return redirect("carrito:carrito")
 
 def limpiar_carrito(request):
     carrito = Carrito(request)
     carrito.limpiar()
-    return redirect("Tienda")
+    return redirect("carrito:carrito")
 
 
 def tienda(request):
@@ -43,4 +43,4 @@ def tienda(request):
     total_carrito = carrito.calcular_total()
     total_carrito = format_currency(total_carrito, 'ARS', locale='es_AR')
 
-    return render(request, "tienda.html", {'productos': productos, 'total_carrito': total_carrito})
+    return render(request, "carrito.html", {'productos': productos, 'total_carrito': total_carrito})
