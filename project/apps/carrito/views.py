ea4ceb580057cfa71b14b8162e59b5166f49e5c4
Tomas Greco <grecotomas06@gmail.com>
2024-02-01 14:37:10 -0300

    Carrito con modelos y cantidades

--- project/apps/carrito/views.py
+++ project/apps/carrito/views.py
@@ -6,15 +6,32 @@ from productos.models import Producto
 
 def carrito(request):
     ids_productos = request.COOKIES.get('carrito', '').split('%2C')
-    cant_productos = request.COOKIES.get('cantidades', '').split('%2C')
-    model_productos = request.COOKIES.get('modelos', '').split('%2C')
+    cantidades = request.COOKIES.get('cantidades', '').split('%2C')
+    modelos = request.COOKIES.get('modelos', '').split('%2C')
     validacion = False
 
     if ids_productos != ['']:
         validacion = True
-        ids_productos = list(map(int, ids_productos))
-        productos_carrito = Producto.objects.filter(id__in=ids_productos)
+        if len(ids_productos) == len(cantidades) == len(modelos):
+            productos_carrito = []
 
-        return render(request, 'carrito.html', {'productos_carrito': productos_carrito, 'validacion': validacion, 'cant_productos' : cant_productos, 'model_productos' : model_productos})
+            for id_producto, cantidad, modelo in zip(ids_productos, cantidades, modelos):
+                producto = Producto.objects.get(id=id_producto)
+                
+                
+                if not modelo:
+                    modelo = "Unico"
+
+                
+                productos_carrito.append({
+                    'producto': producto,
+                    'cantidad': cantidad,
+                    'modelo': modelo,
+                })
+
+                
+            return render(request, 'carrito.html', {'productos_carrito': productos_carrito, 'validacion': validacion})
+        else:
+            return render(request, 'carrito.html', {'validacion': validacion})
     else:
         return render(request, 'carrito.html', {'validacion': validacion})
