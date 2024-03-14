33535c835ad5b8f2dfefe63a9a554624d755920a
Tomas Greco <grecotomas06@gmail.com>
2024-03-14 16:42:47 -0300

    Cambio en settings

--- project/config/settings.py
+++ project/config/settings.py
@@ -134,12 +134,21 @@ THOUSAND_SEPARATOR = "."
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Usar solo en produccion
+"""
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = '/proyectos/MyTime/project/static'
 
 
 MEDIA_ROOT = '/proyectos/MyTime/project/media'
+MEDIA_URL = 'media/'
+"""
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
 
 # Default primary key field type
