a3c051cdbfa88866b4f9783288f8b41a0f84906f
Tomas Greco <grecotomas06@gmail.com>
2024-03-19 19:59:46 -0300

    Panel de administracion v0.2
    (Redirecciones y paneles de categoria y productos. Modificacion con doble click al nombre de la categoria. Progreso en el front)

--- project/apps/administracion/urls.py
+++ project/apps/administracion/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
-    path("administracion-producto", views.administracion, name="adm-producto"),
+    path("administracion-producto", views.AdminProducto, name="adm-producto"),
     path("administracion-categoria", views.AdminCategoria, name="adm-categoria"),
 ]
--- project/apps/administracion/views.py
+++ project/apps/administracion/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, reverse, HttpResponseRedirect, redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from productos.forms import ProductoForm, ImagenProductoFormSet, OpcionFormSet
-from productos.models import Producto
-from productos.forms import ProductoCategoriaForm
+from productos.forms import ProductoForm, ImagenProductoFormSet, OpcionFormSet, ProductoCategoriaForm
+from productos.models import Producto, ProductoCategoria
+from django.http import JsonResponse
 
 
 # Create your views here.
 @login_required
-def administracion(request):
+def AdminProducto(request):
     productos = Producto.objects.all()
     if request.method == 'POST':
         producto_form = ProductoForm(request.POST, request.FILES)
@@ -49,11 +49,19 @@ def administracion(request):
 @login_required
 def AdminCategoria(request):
     if request.method == 'POST':
-        categoria_form = ProductoCategoriaForm(request.POST)
-        if categoria_form.is_valid():
-            categoria_form.save()
-            return redirect('AdminCategoria')  # Redirige a la página de productos después de agregar la categoría
+        categoria_id = request.POST.get('categoria_id')
+        nuevo_nombre = request.POST.get('nuevo_nombre')
+
+        if categoria_id and nuevo_nombre:
+            try:
+                categoria = ProductoCategoria.objects.get(pk=categoria_id)
+                categoria.nombre = nuevo_nombre
+                categoria.save()
+                return JsonResponse({'status': 'ok'})
+            except ProductoCategoria.DoesNotExist:
+                return JsonResponse({'status': 'error', 'message': 'Categoría no encontrada'})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Datos incompletos'})
     else:
-        categoria_form = ProductoCategoriaForm()
-    
-    return render(request, 'categoria_admin.html', {'categoria_form': categoria_form})
+        categorias = ProductoCategoria.objects.all()
+        return render(request, 'categoria_admin.html', {'categorias': categorias})
